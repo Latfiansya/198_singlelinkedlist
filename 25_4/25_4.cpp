f81de048a1af0031c3dc960bc07667baf9689707
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-25 11:59:48 +0700

    Deklarasi fungsi searchNode untuk mencari node dalam linked list berdasar NIM

--- 25_4/25_4.cpp
+++ 25_4/25_4.cpp
@@ -50,3 +50,11 @@ void addNode()
 	nodeBaru->next = current;
 	previous->next = nodeBaru;
 }
+
+
+bool searchNode(int nim, Node* current, Node* previous)
+{
+	previous = START;
+	current = START;
+
+};
